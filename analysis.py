f77f2e8c33a80d17b1d772d446cc3a3cc7daceac
juhalo <107361221+juhalo@users.noreply.github.com>
2022-11-14 00:15:57 +0200

    Use linear regression for bad move detection

--- analysis.py
+++ analysis.py
@@ -1,15 +1,41 @@
 import berserk
+import pandas as pd
+import numpy as np
+from sklearn import linear_model, model_selection, feature_selection
 
 def main(playerName: str, game_type: str, max_game: int, as_pgn: bool):
     white, black = load(playerName, game_type, max_game, as_pgn)
     white_games = []
     black_games = []
-    for times in range(50):
+    for times in range(300):
         game_to_list(white_games, black_games, white, black)
-    print("White:",white_games)
-    print("")
-    print("")
-    print("Black:",black_games)
+    #print("White:",white_games)
+    #print("")
+    #print("")
+    #print("Black:",black_games)
+    df_white = pd.DataFrame(white_games)
+    df_black = pd.DataFrame(black_games)
+    #print(df_white)
+    #print(df_black)
+    print(analyse(df_white))
+    print(analyse(df_black))
+    p_white = analyse2(df_white)
+    p_black = analyse2(df_black)
+    print(p_white.sort_values(by="P"))
+    print(p_black.sort_values(by="P"))
+
+def analyse2(df):
+    p = pd.DataFrame()
+    p["Moves"] = list(df.columns)[1:]
+    p["P"] = feature_selection.f_regression(df[list(df.columns)[1:]], df.winner)[1]
+    return p
+
+def analyse(df):
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(df.drop(['winner'], axis=1), df.winner)
+
+    reg_views = linear_model.LinearRegression().fit(X_train, y_train)
+    return reg_views.score(X_test, y_test)
+
 
 def load(player: str, game: str, max: int, pgn: bool):
     client = berserk.Client()
@@ -22,29 +48,39 @@ def game_to_list(white_games, black_games, white, black):
     white_game = next(white)
     #We care about overextention, therefore moves that take are hard to evaluate as well as pawn moves that are checks
     #Therefore those are ignored, also pushing to 6th rank or further probably means either the pawn was doomed to die anyway or it is going towards queening, both of which are hard to evaluate in terms of overextension.  
-    #If there is no winner, the game was a draw, the generator ignores aborted games automatically
-    white_game_dict = {'winner': 'draw', 'a4': 0, 'a5': 0, 'b4': 0, 'b5': 0, 'c4': 0, 'c5': 0, 'd4': 0, 'd5': 0, 'e4': 0, 'e5': 0, 'f4': 0, 'f5': 0, 'g4': 0, 'g5': 0, 'h4': 0, 'h5': 0}
-    black_game_dict = {'winner': 'draw', 'a4': 0, 'a5': 0, 'b4': 0, 'b5': 0, 'c4': 0, 'c5': 0, 'd4': 0, 'd5': 0, 'e4': 0, 'e5': 0, 'f4': 0, 'f5': 0, 'g4': 0, 'g5': 0, 'h4': 0, 'h5': 0}
-
+    #If there is no winner, the game was a draw, the generator ignores aborted games automatically, here draw=0.5, loss=1, and win=0
+    white_game_dict = {'winner': 0.5, 'a4': False, 'a5': False, 'b4': False, 'b5': False, 'c4': False, 'c5': False, 'd4': False, 'd5': False, 'e4': False, 'e5': False, 'f4': False, 'f5': False, 'g4': False, 'g5': False, 'h4': False, 'h5': False}
+    #black_game_dict = {'winner': 'draw', 'a4': 0, 'a5': 0, 'b4': 0, 'b5': 0, 'c4': 0, 'c5': 0, 'd4': 0, 'd5': 0, 'e4': 0, 'e5': 0, 'f4': 0, 'f5': 0, 'g4': 0, 'g5': 0, 'h4': 0, 'h5': 0}
+    black_game_dict = {'winner': 0.5, 'a4': False, 'a5': False, 'b4': False, 'b5': False, 'c4': False, 'c5': False, 'd4': False, 'd5': False, 'e4': False, 'e5': False, 'f4': False, 'f5': False, 'g4': False, 'g5': False, 'h4': False, 'h5': False}
     #Checks whether or not the starting position is the standard or not, ignores non-standard starting positions 
     if 'initialFen' not in white_game.keys():
         if 'winner' in white_game.keys():
-            white_game_dict['winner'] = white_game['winner']
+            win = white_game['winner']
+            if win == 'white':
+                win = 0
+            else:
+                win = 1
+            white_game_dict['winner'] = win
         white_games_array = white_game['moves'].split()
         for i in range(len(white_games_array)):
             #White starts, indices 0,2,4,.. are white's moves
             if i % 2 == 0 and white_games_array[i] in white_game_dict.keys():
-                white_game_dict[white_games_array[i]] = 1
+                white_game_dict[white_games_array[i]] = True
         white_games.append(white_game_dict)
 
     black_game = next(black)
     if 'initialFen' not in black_game.keys():
         if 'winner' in black_game.keys():
-            black_game_dict['winner'] = black_game['winner']
+            win = black_game['winner']
+            if win == 'black':
+                win = 0
+            else:
+                win = 1
+            black_game_dict['winner'] = win
         black_games_array = black_game['moves'].split()
         for i in range(len(black_games_array)):
             if i % 2 != 0 and black_games_array[i] in black_game_dict.keys():
-                black_game_dict[black_games_array[i]] = 1
+                black_game_dict[black_games_array[i]] = True
         black_games.append(black_game_dict)
 
 
